bd3f08aead89fc45b9030587f4e4c517964cb27c
Daniel Echeverri <echeverri121@gmail.com>
2023-09-11 17:38:59 -0500

    Correct styling so that loading object fits the center vertically and horizontally

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,8 @@
 import './loader.css';
 export default function Loader() {
     return (
-        <div>
+        <main>
             <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-        </div>
+        </main>
     )
 };  
